fc363f46003be07103e5a3a5701941a3e7aa635e
shahadlaml <50049930+shahadlaml@users.noreply.github.com>
2020-06-03 09:09:07 +0300

    the js for the filter is fully working

---
+++ js/filter.js
@@ -0,0 +1,51 @@
+
+//filter toggle btn
+$(document).ready(function(){
+
+  $('.btnweek').click(function(){
+		var attr = $(this).attr('id');
+
+    $('.btnweek').removeClass("active");
+    $(this).addClass("active");
+
+    $('.all').hide();
+    if (attr == 'week1'){
+      $('.'+attr).show();
+    }
+    else if (attr == 'week2'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week3'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week4'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week5'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week6'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week7'){
+      $('.'+attr).show();
+    }
+
+    else if (attr == 'week8'){
+      $('.'+attr).show();
+    }
+    else{
+      $('.all').show();
+    }
+
+
+
+  })
+
+
+});
